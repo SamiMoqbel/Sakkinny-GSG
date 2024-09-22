8bc570da77d36f42772dc2f5e29aa208c84867e5
Sami <moqbelsami@gmail.com>
2024-09-22 19:41:04 +0300

    Login and Signup UIs done

---
+++ frontend/GSG-Sakkiny/src/components/CustomInput/CustomInput.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const CustomInput = ({
+  type,
+  value,
+  placeholder,
+  name,
+  handleChange,
+}) => {
+  return (
+    <input
+      className="w-full p-3 rounded-[20px] border-[##dddddd] border-[1px] text-lg mt-2"
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      name={name}
+      required
+    />
+  );
+};
