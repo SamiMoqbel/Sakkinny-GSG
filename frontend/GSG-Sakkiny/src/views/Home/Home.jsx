a8bbe9d564af87632806de578f2242bac0340180
Sami <moqbelsami@gmail.com>
2024-09-25 21:03:01 +0300

    Working On Auth

--- frontend/GSG-Sakkiny/src/views/Home/Home.jsx
+++ frontend/GSG-Sakkiny/src/views/Home/Home.jsx
@@ -1,14 +1,22 @@
 import { ApartmentCard, SearchBar } from "../../components";
+import { useState } from "react";
 import { Footer } from "../Footer";
 import { Navbar } from "../Navbar";
 
 export const Home = () => {
+
+  const [authenticated, setAuthenticated] = useState(true);
+
   return (
     <div className="w-full h-full flex flex-col">
-      <Navbar />
+      <Navbar authenticated={authenticated} />
       <main className="flex-1 flex flex-col justify-center items-center">
         <SearchBar />
         <section className="flex-1 p-9 flex flex-wrap gap-4 justify-center ">
+          <ApartmentCard />
+          <ApartmentCard />
+          <ApartmentCard />
+          <ApartmentCard />
           <ApartmentCard />
           <ApartmentCard />
           <ApartmentCard />
--- frontend/GSG-Sakkiny/src/views/Navbar/Navbar.jsx
+++ frontend/GSG-Sakkiny/src/views/Navbar/Navbar.jsx
@@ -3,36 +3,43 @@ import { Dropdown } from "flowbite-react";
 import Avatar from "react-avatar";
 import { Logo } from "../../components";
 
-export const Navbar = () => {
+export const Navbar = ({authenticated}) => {
   return (
     <nav className="bg-gray-100 min-h-24 flex items-center justify-between px-10">
       <Logo />
-      <Dropdown
-        label={
-          <Avatar
-            src="http://www.gravatar.com/avatar/a16a38cdfe8b2cbd38e8a56ab93238d3"
-            size="50"
-            round={true}
-          />
-        }
-        arrowIcon={false}
-        inline
-        className="bg-gray-100 text-white rounded-md"
-      >
-        <Dropdown.Header>
-          <span className="block text-sm">Bonnie Green</span>
-          <span className="block truncate text-sm font-medium">
-            name@flowbite.com
-          </span>
-        </Dropdown.Header>
-        <Dropdown.Item>Dashboard</Dropdown.Item>
-        <Dropdown.Item>Settings</Dropdown.Item>
-        <Dropdown.Item>Earnings</Dropdown.Item>
-        <Dropdown.Divider />
-        <Dropdown.Item>
-          <Link to="/login">Sign out</Link>
-        </Dropdown.Item>
-      </Dropdown>
+
+      {authenticated ? (
+        <Dropdown
+          label={
+            <Avatar
+              src="http://www.gravatar.com/avatar/a16a38cdfe8b2cbd38e8a56ab93238d3"
+              size="50"
+              round={true}
+            />
+          }
+          arrowIcon={false}
+          inline
+          className="bg-gray-100 text-white rounded-md"
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>
+            <Link to="/login">Sign out</Link>
+          </Dropdown.Item>
+        </Dropdown>
+      ) : (
+        <Link to="/login" className="text-lg font-medium text-gray-700">
+          Sign in
+        </Link>
+      )}
     </nav>
   );
 };
