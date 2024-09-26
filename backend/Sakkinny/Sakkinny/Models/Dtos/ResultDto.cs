629b5bb6638f7fa8d055c6dd11a425bdc2f067af
Muhnnad Qatanani <muhnnadqatanani123@gmail.com>
2024-09-26 19:29:06 +0300

    add rent

---
+++ backend/Sakkinny/Sakkinny/Models/Dtos/ResultDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sakkinny.Models.Dtos
+{
+    public class ResultDto
+    {
+        
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
