53190d8c55bbf4d602c4d2ee15cd8732fd6f9913
Muhnnad Qatanani <muhnnadqatanani123@gmail.com>
2024-09-28 09:03:13 +0300

    conflect

--- backend/Sakkinny/Sakkinny/obj/Debug/net8.0/Sakkinny.AssemblyInfo.cs
+++ backend/Sakkinny/Sakkinny/obj/Debug/net8.0/Sakkinny.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sakkinny")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089320774222303966814c82de7f19fd04b563fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461f7009c16ae667b67e98fd8fb880d1a66c0992")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sakkinny")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sakkinny")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
