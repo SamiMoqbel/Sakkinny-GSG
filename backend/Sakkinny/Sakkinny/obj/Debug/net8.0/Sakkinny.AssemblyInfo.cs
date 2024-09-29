b88c168b5282a4ce364e51c078815f98e081eccf
Anas-Thaher <anas.thaher000@gmail.com>
2024-09-29 21:58:48 +0300

    solve bug : remove userName from register

--- backend/Sakkinny/Sakkinny/obj/Debug/net8.0/Sakkinny.AssemblyInfo.cs
+++ backend/Sakkinny/Sakkinny/obj/Debug/net8.0/Sakkinny.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sakkinny")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8cb4cc2246553221a747cd0e3b9e81b2691f9ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0fba289606a431a147c1adf4d008ac21d2ca937")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sakkinny")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sakkinny")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
